ca509d6f36dc2c532840cfab038a4879772a4d74
sebaortega <ortegamolarosebastian@gmail.com>
2023-02-15 17:49:41 -0300

    EntregaDenoQuieroSeguirConLaFinal

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
+const port = 8081;
+
+interface IPOSTBody {
+name: string;
+lastName: string;
+}
+
+const handler = async (req: Request): Promise<Response> => {
+if (req.method == "POST") {
+console.log(req.headers);
+// * application/json
+if (
+    req.headers.has("content-type") &&
+    req.headers.get("content-type")?.startsWith("application/json")
+) {
+    const body: IPOSTBody = await req.json();
+    console.log(body);
+    return new Response(JSON.stringify({ data: body }), {
+    status: 200,
+    headers: {
+        "content-type": "application/json; charset=UTF-8",
+    },
+    });
+}
+return new Response("Error! Esperaba un formato json", { status: 200 });
+} else {
+return new Response(
+    `<html><body><h1>Hola Lucía!</h1> <form method="POST"><input type="text" name="name" / ></form></html>`,
+    {
+    headers: {
+        "content-type": "text/html; charset=UTF-8",
+    },
+    }
+    );
+  }
+};
+await serve(handler, { port });
